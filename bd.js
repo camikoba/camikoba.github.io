2cb6a362c06b1cfea1cc2a5fa2747712a942be59
camikoba <camila_kobayashi@outlook.com>
2022-11-08 11:48:40 -0300

    edição no banco de dados

--- bd.js
+++ bd.js
@@ -5,12 +5,14 @@ bd.transaction(function (criar) {
 });
 
 function salvarInfo() {
-    const nomeUsuario = document.getElementById("nome-usuario").value.toUpperCase();
+    const nomeUsuario = document
+        .getElementById("nome-usuario")
+        .value.toUpperCase();
     const idadeUsuario = parseInt(
         document.getElementById("idade-usuario").value
     );
 
-    if (nomeUsuario === "" || isNaN(idadeUsuario)){
+    if (nomeUsuario === "" || isNaN(idadeUsuario)) {
         alert("Faltam informações!");
         return false;
     }
@@ -26,8 +28,10 @@ function salvarInfo() {
 }
 
 function pesquisaPorNome() {
-    const nomeUsuario = document.getElementById("pesquisa-nome-usuario").value.toUpperCase();
-    if (nomeUsuario === ""){
+    const nomeUsuario = document
+        .getElementById("pesquisa-nome-usuario")
+        .value.toUpperCase();
+    if (nomeUsuario === "") {
         alert("Falta informação!");
         return false;
     }
@@ -40,30 +44,33 @@ function pesquisaPorNome() {
 
                 const msg = tamanho + " linhas encontradas";
                 console.log(msg);
-            
+
                 const nome = resultados.rows.item(tamanho - 1).nome;
                 const idade = resultados.rows.item(tamanho - 1).idade;
                 document.getElementById("pesquisa-nome-usuario").value = nome;
-                document.getElementById("resultado-pesquisa").value = nome +", " + idade +" anos";
-            }    
+                document.getElementById("resultado-pesquisa").value =
+                    nome + ", " + idade + " anos";
+            }
         );
     });
 }
 
 function exibeBD() {
     bd.transaction(function (exibe) {
-        exibe.executeSql("SELECT * FROM formulario", [], 
-        function (exibe, resultados) {
-            const tamanho = resultados.rows.length;
-            let item;
-            document.getElementById("lista-bd").innerHTML = "";
-            for (let i = 0; i < tamanho; i++){
-                item = resultados.rows.item(i);
-                document.getElementById("lista-bd").innerHTML += `<p>Nome: ${item.nome}, ${item.idade} anos</p>`
-        
+        exibe.executeSql(
+            "SELECT * FROM formulario",
+            [],
+            function (exibe, resultados) {
+                const tamanho = resultados.rows.length;
+                let item;
+                document.getElementById("lista-bd").innerHTML = "";
+                for (let i = 0; i < tamanho; i++) {
+                    item = resultados.rows.item(i);
+                    document.getElementById(
+                        "lista-bd"
+                    ).innerHTML += `<p>Nome: ${item.nome}, ${item.idade} anos</p>`;
+                }
             }
-       
-
-        });
+        );
     });
 }
