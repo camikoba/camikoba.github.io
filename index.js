8928717861042dbd6401114314c7eea9c87e14ce
Camila Kobayashi <31277885+camikoba@users.noreply.github.com>
2023-09-22 20:53:20 -0300

    Graus F
    sem espaço

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function converteParaFahrenheit(){
     const temperaturaCelsius = parseFloat(document.getElementById("temperatura-celsius").value);
     const temperaturaFahrenheit = temperaturaCelsius * 1.8 + 32;
 
-    document.getElementById("resultado-fahrenheit").value = temperaturaFahrenheit.toFixed(2) +"° F";
+    document.getElementById("resultado-fahrenheit").value = temperaturaFahrenheit.toFixed(2) +" °F";
 
 }
 
